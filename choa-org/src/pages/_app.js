c9aba4e83d29ee2e325d231882f8d0bd3cf212e4
dkobrin <shdwkat999@yahoo.com>
2023-12-05 16:40:12 -0500

    fixed styled components setup

--- choa-org/src/pages/_app.js
+++ choa-org/src/pages/_app.js
@@ -1,10 +1,33 @@
 import Layout from '@/components/layout'
 import '@/styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from '@/styles/globalstyles';
+
+
+const theme = {
+  colors: {
+    powderWhite: "#FFFDF9",
+    persianGreen: "#06B49A",
+    lightBlue: "#AFDBD2",
+    onyx: "#36313D"
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em"
+  }
+};
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   )
 }
