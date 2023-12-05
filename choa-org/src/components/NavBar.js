46e7578545ab4c89b160c1fc91625a5cad546afa
lucas richards dev <lucasrichardsdev@gmail.com>
2023-12-05 14:07:16 -0800

    navbar changes

--- choa-org/src/components/NavBar.js
+++ choa-org/src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Nav = styled.nav`
-  display: flex;
+  display: flex;                      
   justify-content: space-between;
   align-items: center;
   background-color: rgba(83,170,83,1);
@@ -79,28 +80,28 @@ const NavBar = () => {
       </Logo>
       <MobileNavToggle onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
         {isMobileNavOpen ? 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <title>Close Mobile menu</title>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
             : 
-            <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <title>Mobile menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
             </svg>
         }
       </MobileNavToggle>
       <DesktopNav>
-        <NavItem><a href="/">Home</a></NavItem>
-        <NavItem><a href="/">About</a></NavItem>
-        <NavItem><a href="/">Services</a></NavItem>
-        <NavItem><a href="/">Contact</a></NavItem>
+        <NavItem><Link href="/">Home</Link></NavItem>
+        <NavItem><Link href="/">About</Link></NavItem>
+        <NavItem><Link href="/">Services</Link></NavItem>
+        <NavItem><Link href="/">Contact</Link></NavItem>
       </DesktopNav>
       <MobileNav style={{ display: isMobileNavOpen ? 'flex' : 'none' }}>
-        <NavItem><a href="/">Home</a></NavItem>
-        <NavItem><a href="/">About</a></NavItem>
-        <NavItem><a href="/">Services</a></NavItem>
-        <NavItem><a href="/">Contact</a></NavItem>
+      <NavItem><Link href="/">Home</Link></NavItem>
+        <NavItem><Link href="/">About</Link></NavItem>
+        <NavItem><Link href="/">Services</Link></NavItem>
+        <NavItem><Link href="/">Contact</Link></NavItem>
       </MobileNav>
     </Nav>
   );
--- choa-org/src/components/layout.js
+++ choa-org/src/components/layout.js
@@ -1,4 +1,4 @@
-import NavBar from "./NavBar";
+import NavBar from "./navbar";
 import Footer from "./footer";
 
 
