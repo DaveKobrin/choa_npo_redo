6cf4918fb82a0328cfc5efdddb1123d4799e2b0c
lucas richards dev <lucasrichardsdev@gmail.com>
2023-12-05 11:45:30 -0800

    navbar

---
+++ choa-org/src/pages/nav_bar.js
@@ -0,0 +1,11 @@
+import NavBar from "@/components/NavBar";
+
+const Navigation = () => {
+    return(
+        <div>
+            <NavBar />
+        </div>
+    )
+    }
+    
+export default Navigation;
