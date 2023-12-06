ec830ffbe20b8c09db8a82b07f0e62ff0a15a52b
dkobrin <shdwkat999@yahoo.com>
2023-12-06 18:46:43 -0500

    started footer styles and added fav.ico

---
+++ choa-org/src/styles/footerstyles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { PageColors } from './globalstyles';
+
+
+export const FooterContainerWrapper = styled.footer`
+    width: 100vw;
+    padding: 3rem 1rem 2rem 1rem;
+    background-color: ${PageColors.MEDIUM_GRAY};
+    color: ${PageColors.TEXT_WHITE};
+    border: 1px solid red;
+`;
+
+export const FooterContainer = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0 1rem;
+    border: 1px solid red;
+`;
+
+export const LogoWrapper = styled.div`
+
+`;
+
+export const FootUpperNav = styled.div`
+
+`;
