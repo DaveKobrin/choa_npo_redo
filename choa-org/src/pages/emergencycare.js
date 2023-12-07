94ce7257b4ec324b82ab5be6a5ecd3fab0e419af
CrissCross56 <grant.terdoslavich@gmail.com>
2023-12-07 18:57:14 -0500

    fleshing out emergency care page

---
+++ choa-org/src/pages/emergencycare.js
@@ -0,0 +1,92 @@
+import { locationAndWaitTimes } from "@/data/locationAndWaitTimes";
+import { WaitCard } from "@/components/waitCard";
+import { Heading3Title, ParagText, ParagList } from "@/styles/textStyles";
+import { About, EverythingText, Treatment, Photowrap } from "@/styles/emergencystyles";
+import Patient from "../../public/patient.png"
+import Image from "next/image";
+
+export default  function EmergencyCare ()
+{
+    return(
+    <>
+       
+
+         <div className="flex flex-col justify-center mt-32 mb-20">
+          {locationAndWaitTimes
+            .filter((location) => {
+              return location.facilityType === "Emergency Department";
+            })
+            .map((location, index) => (
+              <WaitCard key={index} location={location} />
+            ))}
+        </div>
+        
+<About>
+
+        <Photowrap>
+            <Image src={Patient}/>
+        </Photowrap>
+
+        <EverythingText>
+
+        <Treatment>
+            <Heading3Title>
+                What do We Treat in Our Emergency Department
+            </Heading3Title>
+
+        <ParagText>
+            The Emergency Departments at Children’s Healthcare of Atlanta serve children from birth to age 18 and are open 24 hours a day, seven days a week. No appointments are necessary to bring your baby, child or teen in.
+        </ParagText>
+
+        <ParagText>
+            Call 911 immediately if you think your child’s illness or injury is life-threatening.
+        </ParagText>
+        {/* drop down goes here VVV */}
+        </Treatment>
+
+
+        <Heading3Title>
+            Emergency Department or Urgent Care Center?
+        </Heading3Title>
+
+        <ParagText>
+            Learn more about when to take your child to the emergency department instead of an urgent care center.
+        </ParagText>
+        <button>Learn More</button>
+
+        <Heading3Title>
+            Why Should I Take My Child to a Children’s Emergency Department?
+        </Heading3Title>
+
+        <ParagText>
+            Children are not small adults. Unlike most ERs, our pediatric Emergency Departments at Children’s have doctors and nurses who are board certified and specially trained in caring for children with urgent healthcare needs.
+        </ParagText>
+
+        <ParagText>
+            Our equipment and supplies, from arm slings to blood pressure cuffs, are designed specifically for kids of all sizes, and we know how to make you and your child as comfortable as possible during a difficult time.
+        </ParagText>
+
+        <h3>Advanced Trauma Care</h3>
+
+        <ParagText>
+            We combine advanced technology with dedicated, highly skilled physicians and staff to provide care for pediatric trauma patients:
+        </ParagText>
+
+        <h3>Transport Services</h3>
+
+        <ParagText>
+        When seconds count, Children’s has a pediatric-trained transport team to transfer critical patients to our
+        three hospitals for treatment. Our transport team members have some of the most progressive training in the
+        industry. They are trained to understand pediatric patients’ needs and the physical and emotional differences
+        that make treating babies, children and teens unique.
+        </ParagText>
+
+        {/* see more dropdown  */}
+
+        {/* see more dropdownlist */}
+        </EverythingText>
+
+    </About>
+</>
+    )
+}
---
+++ choa-org/src/styles/emergencystyles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Treatment = styled.div`
+display: flex;
+
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+gap: 19px;
+`
+export const EmergencyUrgent = styled.div`
+flex-direction: column;
+justify-content: center;
+align-items: space-evenly;
+`
+export const ChildEmergency = styled.div`
+flex-direction: column;
+justify-content: center;
+align-items: space-evenly;
+`
+
+export const EverythingText = styled.div`
+// display: flex;
+// flex-direction: column;
+// justify-content: center;
+// width: 60vw;
+// margin: 1rem auto;
+margin-left: 64px;
+`
+export const PageHalves = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+`
+
+export const About = styled.div`
+display: flex;
+flex-direction: row-reverse;
+gap: 500px;
+`
+export const Photowrap = styled.div`
+
+background-size: cover;
+background-repeat: no-repeat;
+background-position: center;
+height: 500px;
+margin-top: 10px;
+position: relative;
+`
