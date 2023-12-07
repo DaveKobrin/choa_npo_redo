248209d334f4a73a596747147baef5fd62017a7b
asp <amandasueputney@gmail.com>
2023-12-07 10:31:08 -0800

    adds all pages and updates index page styling

--- choa-org/src/pages/index.js
+++ choa-org/src/pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import React from 'react';
-import { HeroSection, HeroCard, HeroContent, CardImage} from '../styles/indexherostyles';
+import { HeroSection, HeroCard, HeroContent, CardImage, MiddleDiv, ImageContainer} from '../styles/indexherostyles';
 import Dropdown from '../components/ImportantUpdate';
 
 
@@ -20,11 +20,16 @@ export default function Home() {
     <HeroSection>
 
         <HeroContent>
-          <h1>Nothing Matters More Than Kids</h1>
-          <p>Every day we provide nationally-ranked 
+          <h2 className="mb-5 text-#285844 text-4xl">Nothing Matters More Than Kids</h2>
+          <p className="text-#5A5A5A">Every day we provide nationally-ranked 
           pediatric care to Georgia’s kids and teens,
           so they can get back to just being kids.</p>
         </HeroContent>
+         <MiddleDiv>
+            <ImageContainer>
+              <Image src="/usnews_top_ranking.png" width="145" height="203" />
+             </ImageContainer>
+        </MiddleDiv>
 
          <HeroCard>
            <CardImage src="/indexHeroBanner.png" alt="Baby Girl" />
