6105baa2511c6def830d9117105cb58d0958e4eb
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-12-07 09:44:01 -0600

    cleaning up and starting out wait times page

--- choa-org/src/pages/wait_times.js
+++ choa-org/src/pages/wait_times.js
@@ -1,14 +1,25 @@
 import { WaitCard } from "@/components/waitCard";
 import {locationAndWaitTimes} from "@/data/locationAndWaitTimes";
 
+// EXAMPLE:
+// locationAndWaitTimes.map((location, idx) => {
+//     return (
+//     <>
+//         <WaitCard location={location} key={idx} />
+//     </>
+// )})
+
+
 const WaitTimes = () => {
     return (
-        locationAndWaitTimes.map((location, idx) => {
-            return (
-            <>
-                <WaitCard location={location} key={idx} />
-            </>
-        )})
+        <>
+            <div className="min-w-screen h-screen">
+
+                    <h1>Wait times</h1>
+                    
+            </div>
+        
+        </>
     )
 }
 
